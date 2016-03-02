78773fcd06ee9bc203451a250b483720a12addcd
toni_heittola <toni.heittola@gmail.com>
2016-03-03 00:37:52 +0200

    add wav-file example

--- tests/visualization_test.py
+++ tests/visualization_test.py
@@ -1,6 +1,6 @@
 import sed_vis
 
-audio, fs = sed_vis.io.load_audio('data/a001.flac')
+audio, fs = sed_vis.io.load_audio('data/a001.wav')
 vis = sed_vis.visualization.EventListVisualizer(event_lists={'reference': sed_vis.io.load_event_list('data/a001.ann'),
                                                              'full': sed_vis.io.load_event_list('data/a001_full.ann'),
                                                              'estimated': sed_vis.io.load_event_list('data/a001_system_output.ann')},
