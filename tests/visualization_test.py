1eb453a311529e0aea9074f78f399d78b3b88606
heittolt <toni.heittola@gmail.com>
2016-03-03 14:07:05 +0200

    more robust file handling

--- tests/visualization_test.py
+++ tests/visualization_test.py
@@ -1,9 +1,11 @@
 import sed_vis
+import os
 
-audio, fs = sed_vis.io.load_audio('data/a001.wav')
-vis = sed_vis.visualization.EventListVisualizer(event_lists={'reference': sed_vis.io.load_event_list('data/a001.ann'),
-                                                             'full': sed_vis.io.load_event_list('data/a001_full.ann'),
-                                                             'estimated': sed_vis.io.load_event_list('data/a001_system_output.ann')},
+current_path = os.path.dirname(os.path.realpath(__file__))
+audio, fs = sed_vis.io.load_audio(os.path.join(current_path,'data/a001.wav'))
+vis = sed_vis.visualization.EventListVisualizer(event_lists={'reference': sed_vis.io.load_event_list(os.path.join(current_path,'data/a001.ann')),
+                                                             'full': sed_vis.io.load_event_list(os.path.join(current_path,'data/a001_full.ann')),
+                                                             'estimated': sed_vis.io.load_event_list(os.path.join(current_path,'data/a001_system_output.ann'))},
                                                 audio_signal=audio,
                                                 sampling_rate=fs)
 
