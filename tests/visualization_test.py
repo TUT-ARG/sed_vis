7233cb18f02349d9ee2356f58dd658ac4d69d85b
Toni Heittola <toni.heittola@gmail.com>
2017-10-13 11:29:52 +0300

    Add examples how to visualize multiple system outputs at the same time.

--- tests/visualization_test.py
+++ tests/visualization_test.py
@@ -2,7 +2,7 @@
 import sed_vis
 import os
 
-mode = 'dcase2016'
+mode = 'multiple' #''dcase2016'
 current_path = os.path.dirname(os.path.realpath(__file__))
 
 if mode == 'dcase2016':
@@ -47,4 +47,21 @@ elif mode == 'sync':
                                                     mode='time_domain'
                                                     )
 
+    vis.show()
+
+elif mode == 'multiple':
+    # Test visualization of multiple system outputs
+    audio, fs = sed_vis.io.load_audio(os.path.join(current_path, 'data/a001.wav'))
+    vis = sed_vis.visualization.EventListVisualizer(event_lists={'reference': sed_vis.io.load_event_list(os.path.join(current_path,'data/a001.ann')),
+                                                                 'estimated1': sed_vis.io.load_event_list(os.path.join(current_path,'data/a001_system_output.ann')),
+                                                                 'estimated2': sed_vis.io.load_event_list(os.path.join(current_path,'data/a001_system_output_2.ann'))},
+                                                    event_list_order=['reference', 'estimated1', 'estimated2'],
+                                                    audio_signal=audio,
+                                                    sampling_rate=fs,
+                                                    spec_cmap='jet',
+                                                    spec_interpolation='bicubic',
+                                                    spec_win_size=1024,
+                                                    spec_hop_size=1024/8,
+                                                    spec_fft_size=1024,
+                                                    publication_mode=True)
     vis.show()
