c6afe12ea55143515328fdf8d9bafe0f1ef97294
Toni Heittola <toni.heittola@gmail.com>
2023-11-23 15:04:33 +0200

    Add event_roll draft to the video generator

--- sed_vis/video.py
+++ sed_vis/video.py
@@ -161,13 +161,13 @@ class VideoGenerator(object):
             'event_roll': {
                 'enable': True,
                 'size': {
-                    'width': self.frame_width,
+                    'width': 660,
                     'height': 300,
                 },
                 'top_y': 370,
                 'top_x': 0,
-                'color': (255, 255, 255),
-                'frame_color': None
+                'color': (155, 155, 155),
+                'frame_color': (100,100,100)
             }
         }
         self.panels['footer']['size']['width'] = self.frame_width
@@ -178,6 +178,7 @@ class VideoGenerator(object):
             self.panels['spectrogram']['top_x'] = 10
         else:
             self.panels['spectrogram']['top_x'] = self.panels['video']['size']['width'] + 50
+            self.panels['event_roll']['top_x'] = self.panels['video']['size']['width'] + 50
 
         self.text_labels = {
             'spectrogram': 'Audio spectrogram',
@@ -193,13 +194,19 @@ class VideoGenerator(object):
         self.font = cv2.FONT_HERSHEY_SIMPLEX
 
         self.spectrogram_move = 10
-        self.spectrogram_segment_duration = self.panels['spectrogram']['size']['width'] / self.spectrogram_move * self.frame_duration
+        self.spectrogram_segment_duration = (self.panels['spectrogram']['size']['width']-6) / self.spectrogram_move * self.frame_duration
         self.cmap = matplotlib.cm.get_cmap('magma')
         self.norm = matplotlib.colors.Normalize(vmin=-20, vmax=20)
 
         self.spectrogram = numpy.zeros((self.panels['spectrogram']['size']['height']-6, self.panels['spectrogram']['size']['width']-6, 3), numpy.uint8)
         self.spectrogram.fill(0)
         self.fft_size = 1024
+
+        self.event_roll_move = 10
+        self.event_roll_segment_duration = (self.panels['event_roll']['size']['width']-6) / self.event_roll_move * self.frame_duration
+        self.event_roll = numpy.zeros((self.panels['event_roll']['size']['height']-6, self.panels['event_roll']['size']['width']-6, 3), numpy.uint8)
+        self.event_roll.fill(0)
+
 
         self.ui.section_header('Video generator')
         self.ui.data('Source', self.source_video)
@@ -292,13 +299,18 @@ class VideoGenerator(object):
         self.spectrogram[:, video_spectrogram_offset:, :] = video_spectrogram[:, 0:video_spectrogram_offset, :]
 
         if self.panels['event_roll']['enable']:
-            event_roll = numpy.zeros((self.panels['event_roll']['size']['width'], self.panels['event_roll']['size']['height'], 3), numpy.uint8)
-            event_roll.fill(255)
+            video_event_roll = numpy.zeros((self.panels['event_list']['size']['height']-6, int(self.audio_data.duration_sec*1000), 3), numpy.uint8)
+            video_event_roll.fill(255)
 
             line_margin = 0.1
             y = 0
             event_list_count = len(self._event_lists)
             annotation_height = (1.0-line_margin*2)/event_list_count
+
+            label_lane_height = int((video_event_roll.shape[0]) / self.event_label_count)
+
+            sublane_margin = 3
+            sublane_height = int(label_lane_height / len(self._event_list_order)-sublane_margin)
 
             if event_list_count == 1:
                 norm = colors.Normalize(
@@ -314,12 +326,13 @@ class VideoGenerator(object):
                 norm=norm,
                 cmap=self.label_colormap
             )
-            embed()
+
             for label in self.active_events:
                 for event_list_id, event_list_label in enumerate(self._event_list_order):
                     offset = (len(self._event_list_order)-1-event_list_id) * annotation_height
 
-                    event_y = y - 0.5 + line_margin + offset
+                    event_y1 = y + (event_list_id * (sublane_height+sublane_margin)) #int((y - 0.5 + line_margin + offset)* 10)
+                    event_y2 = y + (event_list_id * (sublane_height+sublane_margin)) + sublane_height #label_lane_height #int((y - 0.5 + line_margin + offset + annotation_height) * 10 )
 
                     # grid line
                     for event in self._event_lists[event_list_label]:
@@ -330,41 +343,28 @@ class VideoGenerator(object):
                                 color = m.to_rgba(y + offset)
                             else:
                                 color = m.to_rgba(event_list_id)
+                            color = (int(color[0] * 255), int(color[1] * 255), int(color[2] * 255))
 
-                            #rectangle = plt.Rectangle(
-                            #    (event['onset'], event_y),
-                            #    height=annotation_height,
-                            #    width=event_length,
-                            #    edgecolor='black',
-                            #    facecolor=color,
-                            #    linewidth=0,
-                            #    alpha=self.event_roll_item_opacity,
-                            #    picker=5
-                            #)
-                            event_roll = cv2.rectangle(
-                                img=event_roll,
-                                pt1=(event['onset'], event_y),
-                                pt2=(event['onset']+event_length, event_y+annotation_height),
+                            video_event_roll = cv2.rectangle(
+                                img=video_event_roll,
+                                pt1=(int(event['onset']*1000), event_y1),
+                                pt2=(int(event['onset']*1000) + int(event_length*1000), event_y2),
                                 color=color,
                                 thickness=-1
                             )
 
+                y += label_lane_height
+            video_event_roll = cv2.resize(
+                src=video_event_roll,
+                dsize=(self.frame_count*self.event_roll_move, self.panels['event_roll']['size']['height'] - 6),
+                interpolation=cv2.INTER_AREA
+            )
+
+            video_event_roll_offset = int(self.event_roll.shape[1] / 2)
+            self.event_roll[:, video_event_roll_offset:, :] = video_event_roll[:, 0:video_event_roll_offset, :]
 
 
         while True:
-            self.spectrogram = self.move_image(image_data=self.spectrogram, x=-self.spectrogram_move, y=0)
-
-            spectrogram_slice_start = video_spectrogram_offset + frame_index * self.spectrogram_move
-            spectrogram_slice_end = video_spectrogram_offset + (frame_index+1) * self.spectrogram_move
-
-            if spectrogram_slice_start >= video_spectrogram.shape[1] or spectrogram_slice_end >= video_spectrogram.shape[1]:
-                spectrogram_slice = numpy.zeros((video_spectrogram.shape[0], self.spectrogram_move, 3), numpy.uint8)
-            else:
-                spectrogram_slice = video_spectrogram[:, spectrogram_slice_start:spectrogram_slice_end, :]
-
-            if spectrogram_slice.shape[1] > 0:
-                self.spectrogram[:, -(self.spectrogram_move + 1):-1, :] = spectrogram_slice[:, :, :]
-
             video_ret, video_frame = self.video_data.read()
             if video_ret:
                 video_frame = self.image_resize(
@@ -442,6 +442,19 @@ class VideoGenerator(object):
                     )
 
             if self.panels['spectrogram']['enable']:
+                self.spectrogram = self.move_image(image_data=self.spectrogram, x=-self.spectrogram_move, y=0)
+
+                spectrogram_slice_start = video_spectrogram_offset + frame_index * self.spectrogram_move
+                spectrogram_slice_end = video_spectrogram_offset + (frame_index + 1) * self.spectrogram_move
+
+                if spectrogram_slice_start >= video_spectrogram.shape[1] or spectrogram_slice_end >= video_spectrogram.shape[1]:
+                    spectrogram_slice = numpy.zeros((video_spectrogram.shape[0], self.spectrogram_move, 3), numpy.uint8)
+                else:
+                    spectrogram_slice = video_spectrogram[:, spectrogram_slice_start:spectrogram_slice_end, :]
+
+                if spectrogram_slice.shape[1] > 0:
+                    self.spectrogram[:, -(self.spectrogram_move + 1):-1, :] = spectrogram_slice[:, :, :]
+
                 # Spectrogram box
                 current_output_frame = cv2.rectangle(
                     img=current_output_frame,
@@ -482,11 +495,11 @@ class VideoGenerator(object):
                 )
 
                 freq_index = 0
-                tick_hz = 16000 / 4.0
+                tick_hz = self.fs / 4.0
                 for y in range(self.panels['spectrogram']['top_y'], self.panels['spectrogram']['top_y'] + self.spectrogram.shape[0] + 1, int(self.spectrogram.shape[0] / 4)):
                     cv2.putText(
                         img=current_output_frame,
-                        text='{index:.0f}kHz'.format(index=(16000 - freq_index * tick_hz) / 1000),
+                        text='{index:.0f}kHz'.format(index=(self.fs - freq_index * tick_hz) / 1000),
                         org=(self.panels['spectrogram']['top_x'] + self.spectrogram.shape[1] + 10, y + 4),
                         fontFace=self.font,
                         fontScale=0.5,
@@ -680,7 +693,68 @@ class VideoGenerator(object):
                         current_text_y += text_height + row_margin
 
             if self.panels['event_roll']['enable']:
-                pass
+                self.event_roll = self.move_image(image_data=self.event_roll, x=-self.event_roll_move, y=0)
+
+                event_roll_slice_start = video_event_roll_offset + frame_index * self.event_roll_move
+                event_roll_slice_end = video_event_roll_offset + (frame_index + 1) * self.event_roll_move
+
+                if event_roll_slice_start >= video_event_roll.shape[1] or event_roll_slice_end >= video_event_roll.shape[1]:
+                    event_roll_slice = numpy.zeros((video_event_roll.shape[0], self.event_roll_move, 3), numpy.uint8)
+                else:
+                    event_roll_slice = video_event_roll[:, event_roll_slice_start:event_roll_slice_end, :]
+
+                if event_roll_slice.shape[1] > 0:
+                    self.event_roll[:, -(self.event_roll_move + 1):-1, :] = event_roll_slice[:, :, :]
+
+                current_output_frame = cv2.rectangle(
+                    img=current_output_frame,
+                    pt1=(self.panels['event_roll']['top_x'], self.panels['event_roll']['top_y']),
+                    pt2=(self.panels['event_roll']['top_x'] + self.panels['event_roll']['size']['width'],
+                         self.panels['event_roll']['top_y'] + self.panels['event_roll']['size']['height']),
+                    color=self.panels['event_roll']['color'],
+                    thickness=-1
+                )
+
+                if self.panels['event_roll']['frame_color']:
+                    current_output_frame = cv2.rectangle(
+                        img=current_output_frame,
+                        pt1=(self.panels['event_roll']['top_x'], self.panels['event_roll']['top_y']),
+                        pt2=(self.panels['event_roll']['top_x'] + self.panels['event_roll']['size']['width'],
+                             self.panels['event_roll']['top_y'] + self.panels['event_roll']['size']['height']),
+                        color=self.panels['event_roll']['frame_color'],
+                        thickness=3
+                    )
+
+                # Place spectrogram
+                offset_y = int((self.panels['event_roll']['size']['height'] - self.event_roll.shape[0]) / 2)
+                offset_x = int((self.panels['event_roll']['size']['width'] - self.event_roll.shape[1]) / 2)
+                self.place_image(
+                    target_image=current_output_frame,
+                    source_image=self.event_roll,
+                    x=offset_x + self.panels['event_roll']['top_x'],
+                    y=offset_y + self.panels['event_roll']['top_y']
+                )
+                shade = numpy.zeros_like(current_output_frame, numpy.uint8)
+
+                cv2.rectangle(
+                    img=shade,
+                    pt1=(
+                        self.panels['event_roll']['top_x'] + int(self.panels['event_roll']['size']['width'] / 2),
+                        self.panels['event_roll']['top_y'] + 3
+                    ),
+                    pt2=(
+                        self.panels['event_roll']['top_x'] + self.panels['event_roll']['size']['width'] - 3,
+                        self.panels['event_roll']['top_y'] + self.panels['event_roll']['size']['height'] - 3
+                    ),
+                    color=(255, 255, 255),
+                    thickness=cv2.FILLED
+                )
+                mask = shade.astype(bool)
+                current_output_frame_shaded = current_output_frame.copy()
+                alpha = 0.5
+                current_output_frame_shaded[mask] = cv2.addWeighted(current_output_frame, alpha, shade, 1 - alpha, 0)[mask]
+                current_output_frame = current_output_frame_shaded
+
 
             # Wait for keypress
             k = cv2.waitKey(10)
